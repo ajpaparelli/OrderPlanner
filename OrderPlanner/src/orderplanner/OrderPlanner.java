9eba1b610595dde6e3ea4e3bb0f6422240efa406
Adrian Paparelli <adrian.paparelli@mindlinc.com>
2017-10-04 22:06:54 -0400

    A little more work

--- OrderPlanner/src/orderplanner/OrderPlanner.java
+++ OrderPlanner/src/orderplanner/OrderPlanner.java
@@ -13,11 +13,17 @@ import forms.OrderPanel;
 import java.awt.GridLayout;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.BoxLayout;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultListModel;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.DefaultTableModel;
 import utils.Utils;
 
 /**
@@ -32,20 +38,20 @@ public class OrderPlanner extends javax.swing.JFrame {
      * Creates new form OrderPlanner
      */
     public OrderPlanner() {
+        
         initComponents();
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         initDB(); 
         loadTypes();
-        og = new OrderGroup(0);
+        addListener();
+        //og = new OrderGroup(0);
         jComboBox2.setSelectedItem("2");
-        ip = new ItemPanel(db);
-        jPanel2.add(ip);
-        ip.setVisible(true);
 
         
-        jPanel3.add(og);
-        og.setVisible(true);
+        //jPanel3.add(og);
+        //og.setVisible(true);
         
-        ip.loadItems(jComboBox1);
+        loadItems();
         
 //        getContentPane().setLayout(
 //            new BoxLayout(getContentPane(), BoxLayout.Y_AXIS)
@@ -55,7 +61,60 @@ public class OrderPlanner extends javax.swing.JFrame {
 //        ip.setVisible(true);
 //        og.setVisible(true);
     }
-    
+        private void addListener()
+    {
+        jTextField1.getDocument().addDocumentListener(new DocumentListener() {
+
+            @Override
+            public void insertUpdate(DocumentEvent de) {
+                searchItems();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent de) {
+                searchItems();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent de) {
+                searchItems();
+            }
+        });    
+    }
+        
+     private void searchItems()
+    {
+        String SearchStr = jTextField1.getText();
+        String TypeStr = (String) jComboBox1.getSelectedItem();
+        ResultSet rs = db.findItems(TypeStr, SearchStr);
+        Vector<String> columnNames = new Vector<>();
+        Vector<Vector<Object>> dataVector = new Vector<>();
+        try
+        {
+        int columnCount = rs.getMetaData().getColumnCount();
+
+        // Column names.
+
+        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+            columnNames.add(rs.getMetaData().getColumnName(columnIndex));
+        }
+
+        // Data of the table.
+        while (rs.next()) {
+            Vector<Object> rowVector = new Vector<>();
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                rowVector.add(rs.getObject(columnIndex));
+            }
+            dataVector.add(rowVector);
+        }
+        } catch (SQLException ex) {
+            Logger.getLogger(MainForm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        DefaultTableModel dtm = new DefaultTableModel(dataVector, columnNames);
+        jTable1.setModel(dtm);
+    }
+     
     private void initDB()
     {
         db = new DoterraDB(Utils.getCurrentDirectory() + "\\data\\dottera.db");
@@ -63,6 +122,37 @@ public class OrderPlanner extends javax.swing.JFrame {
         db.initialize();
     }
 
+       public void loadItems()
+    {
+        String str = (String) jComboBox1.getSelectedItem();
+        ResultSet rs = db.getItems(str);
+        Vector<String> columnNames = new Vector<>();
+        Vector<Vector<Object>> dataVector = new Vector<>();
+        try
+        {
+        int columnCount = rs.getMetaData().getColumnCount();
+
+        // Column names.
+
+        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+            columnNames.add(rs.getMetaData().getColumnName(columnIndex));
+        }
+
+        // Data of the table.
+        while (rs.next()) {
+            Vector<Object> rowVector = new Vector<>();
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                rowVector.add(rs.getObject(columnIndex));
+            }
+            dataVector.add(rowVector);
+        }
+        } catch (SQLException ex) {
+            Logger.getLogger(MainForm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        DefaultTableModel dtm = new DefaultTableModel(dataVector, columnNames);
+        jTable1.setModel(dtm);
+    }
     
     private void loadTypes()
     {
@@ -97,13 +187,28 @@ public class OrderPlanner extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
         jPanel3 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jPanel4 = new javax.swing.JPanel();
+        orderPanel1 = new forms.OrderPanel();
+        orderPanel2 = new forms.OrderPanel();
+        orderPanel5 = new forms.OrderPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(800, 700));
-        getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.Y_AXIS));
+        getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.PAGE_AXIS));
 
+        jPanel1.setMaximumSize(new java.awt.Dimension(800, 50));
+        jPanel1.setMinimumSize(new java.awt.Dimension(800, 50));
         jPanel1.setPreferredSize(new java.awt.Dimension(800, 50));
+
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
 
         jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6" }));
         jComboBox2.addActionListener(new java.awt.event.ActionListener() {
@@ -127,15 +232,15 @@ public class OrderPlanner extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 67, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(119, 119, 119)
+                .addGap(18, 18, 18)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(277, 277, 277))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -148,25 +253,59 @@ public class OrderPlanner extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(jLabel2)
                     .addComponent(jLabel3))
-                .addContainerGap(18, Short.MAX_VALUE))
+                .addContainerGap(19, Short.MAX_VALUE))
         );
 
         getContentPane().add(jPanel1);
 
         jPanel2.setPreferredSize(new java.awt.Dimension(800, 250));
+        jPanel2.setLayout(new javax.swing.BoxLayout(jPanel2, javax.swing.BoxLayout.LINE_AXIS));
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jTable1.setDragEnabled(true);
+        jTable1.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        jScrollPane1.setViewportView(jTable1);
+
+        jPanel2.add(jScrollPane1);
+
         getContentPane().add(jPanel2);
 
         jPanel3.setPreferredSize(new java.awt.Dimension(800, 375));
-        jPanel3.setLayout(new java.awt.GridLayout(1, 0));
+        jPanel3.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+
+        jScrollPane2.setMinimumSize(new java.awt.Dimension(100, 100));
+
+        jPanel4.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+        jPanel4.add(orderPanel1);
+        jPanel4.add(orderPanel2);
+        jPanel4.add(orderPanel5);
+
+        jScrollPane2.setViewportView(jPanel4);
+
+        jPanel3.add(jScrollPane2);
+
         getContentPane().add(jPanel3);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jComboBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox2ActionPerformed
-        og.resize(Integer.valueOf((String) jComboBox2.getSelectedItem()));
-        this.invalidate();
+        //og.resize(Integer.valueOf((String) jComboBox2.getSelectedItem()));
     }//GEN-LAST:event_jComboBox2ActionPerformed
+
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        loadItems();       
+    }//GEN-LAST:event_jComboBox1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -212,6 +351,13 @@ public class OrderPlanner extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
+    private forms.OrderPanel orderPanel1;
+    private forms.OrderPanel orderPanel2;
+    private forms.OrderPanel orderPanel5;
     // End of variables declaration//GEN-END:variables
 }
